122525eec2799a5b91f6ab243112927f6b74c2f7
Abdulhadi Al-Sayed <ahayed@yahoo.com>
2022-11-21 12:05:11 +0000

    Inital project setup

---
+++ src/main/java/com/sparta/academy/mfix_mongodb_api/MfixMongoDbApiApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.academy.mfix_mongodb_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfixMongoDbApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MfixMongoDbApiApplication.class, args);
+    }
+
+}
