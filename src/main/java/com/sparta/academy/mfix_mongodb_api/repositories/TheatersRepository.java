632a62e8dbf28a20dcf1be5294d5d7872993328f
JamesKempadoo <44989874+JamesKempadoo@users.noreply.github.com>
2022-11-21 17:17:50 +0000

    theater entity/repo/controller changes

--- src/main/java/com/sparta/academy/mfix_mongodb_api/repositories/TheatersRepository.java
+++ src/main/java/com/sparta/academy/mfix_mongodb_api/repositories/TheatersRepository.java
@@ -1,6 +1,7 @@
 package com.sparta.academy.mfix_mongodb_api.repositories;
 
+import com.sparta.academy.mfix_mongodb_api.entity.Theaters;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface TheatersRepository extends MongoRepository <> {
+public interface TheatersRepository extends MongoRepository<Theaters,String>{
 }
--- src/main/java/com/sparta/academy/mfix_mongodb_api/repositories/UsersRepository.java
+++ src/main/java/com/sparta/academy/mfix_mongodb_api/repositories/UsersRepository.java
@@ -1,10 +1,7 @@
 package com.sparta.academy.mfix_mongodb_api.repositories;
 
+import com.sparta.academy.mfix_mongodb_api.entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface UsersRepository extends MongoRepository <>{
-
-
-
-
+public interface UsersRepository extends MongoRepository<User,String>{
 }
