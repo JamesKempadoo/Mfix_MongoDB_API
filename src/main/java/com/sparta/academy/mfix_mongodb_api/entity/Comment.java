6741016f4f962c2874f85fc3177ed5f648856527
Lucas M <lucas.m.marchi@outlook.com>
2022-11-21 16:14:40 +0000

    Comment class

---
+++ src/main/java/com/sparta/academy/mfix_mongodb_api/entity/Comment.java
@@ -0,0 +1,89 @@
+package com.sparta.academy.mfix_mongodb_api.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document( collection = "comments")
+public class Comment {
+
+    @Id
+    @Field
+    public String _id;
+
+    @Field
+    public String name;
+
+    @Field
+    public String date;
+
+    @Field
+    public String email;
+
+    @Field
+    public String movie_id;
+
+    @Field
+    public String text_id;
+
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Object getMovie_id() {
+        return movie_id;
+    }
+
+    public void setMovie_id(String movie_id) {
+        this.movie_id = movie_id;
+    }
+
+    public String getText_id() {
+        return text_id;
+    }
+
+    public void setText_id(String text_id) {
+        this.text_id = text_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "_id=" + _id +
+                ", date='" + date + '\'' +
+                ", email='" + email + '\'' +
+                ", movie_id=" + movie_id +
+                ", text_id='" + text_id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
