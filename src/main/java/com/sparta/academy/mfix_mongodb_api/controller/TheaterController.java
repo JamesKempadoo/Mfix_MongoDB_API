f1f5d4da1915feeeebd6566cff557449ae54f0d2
yoyesu <yoyes15@gmail.com>
2022-11-23 17:47:40 +0000

    WIP post method validation

--- src/main/java/com/sparta/academy/mfix_mongodb_api/controller/TheaterController.java
+++ src/main/java/com/sparta/academy/mfix_mongodb_api/controller/TheaterController.java
@@ -2,11 +2,15 @@ package com.sparta.academy.mfix_mongodb_api.controller;
 
 //import com.sparta.academy.mfix_mongodb_api.entity.Theater;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sparta.academy.mfix_mongodb_api.entity.theater.Location;
 import com.sparta.academy.mfix_mongodb_api.entity.theater.TheaterDTO;
 import com.sparta.academy.mfix_mongodb_api.exceptions.NoTheaterFoundException;
 import com.sparta.academy.mfix_mongodb_api.repositories.TheaterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -40,12 +44,36 @@ public class TheaterController {
     }
 
     @PostMapping("/theaters/new")
-    public TheaterDTO addNewTheater(@RequestBody TheaterDTO theater) {
+    public ResponseEntity<String> addNewTheater(@RequestBody TheaterDTO theater) throws JsonProcessingException {
+        ResponseEntity<String> response;
         if(theaterRepository.findAll().contains(theaterRepository.getTheaterDTOByTheaterId(theater.getTheaterId()))){
-            throw new RuntimeException();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Theater with id " + theater.getTheaterId() + " already exists");
+        } else if(theater.getTheaterId() < 0){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("TheaterId must be equal or greater than 0");
+        } else if(theater.getLocation() == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Location is null");
+        } else if(((theater.getLocation().getGeo().getCoordinates().get(1) >= -90 && theater.getLocation().getGeo().getCoordinates().get(1) <=90) && (theater.getLocation().getGeo().getCoordinates().get(0) >= -180 && theater.getLocation().getGeo().getCoordinates().get(0) <=180))){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Wrong coordinates introduced.");
+        } else if(theater.getLocation().getGeo().getType() == null || theater.getLocation().getGeo().getType().isBlank()){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Wrong geo type introduced.");
+        } else if(!theater.getLocation().getAddress().getZipcode().matches("[0-9]{5}")){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Wrong zipcode introduced.");
+        } else if(theater.getLocation().getAddress().getCity() == null || theater.getLocation().getAddress().getCity().isBlank() || Character.isUpperCase(theater.getLocation().getAddress().getCity().charAt(0))){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Wrong city introduced.");
+        } else if(theater.getLocation().getAddress().getStreet1() == null || theater.getLocation().getAddress().getStreet1().isBlank()){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Street field cannot be empty.");
+        } else if(theater.getLocation().getAddress().getState() == null || theater.getLocation().getAddress().getState().isBlank()){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("State field cannot be empty.");
+        } else if(theater.getId() == null || theater.getId().isBlank()){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("ID field cannot be empty.");
+        } else {
+            theaterRepository.insert(theater);
+            ObjectMapper objectMapper = new ObjectMapper();
+            response = new ResponseEntity<>(objectMapper.writeValueAsString(theater), HttpStatus.OK);
         }
         theaterRepository.save(theater);
-        return theater;
+
+        return response;
     }
 
     @PatchMapping(value = "/theaters/{id}", consumes = "application/json")
