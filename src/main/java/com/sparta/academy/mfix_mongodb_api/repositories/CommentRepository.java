505bc3e59f3800d55d6e97de9293c6a097c28dc6
JamesKempadoo <44989874+JamesKempadoo@users.noreply.github.com>
2022-11-24 16:11:38 +0000

    Made more efficient

--- src/main/java/com/sparta/academy/mfix_mongodb_api/repositories/CommentRepository.java
+++ src/main/java/com/sparta/academy/mfix_mongodb_api/repositories/CommentRepository.java
@@ -3,10 +3,23 @@ package com.sparta.academy.mfix_mongodb_api.repositories;
 import com.sparta.academy.mfix_mongodb_api.model.entity.Comment;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
 
 public interface CommentRepository extends MongoRepository <Comment, String> {
     List<Comment> findCommentByMovieId(String movieId);
     void deleteAllByEmail(String email);
     Comment findCommentById(String id);
+
+    List<Comment> findCommentByTextContaining(String text);
+
+    List<Comment> findCommentByNameContaining(String name);
+
+    List<Comment> findCommentByDateBetween(LocalDateTime beforeTime, LocalDateTime afterTime);
+
+    List<Comment> findCommentByEmail(String email);
+
+
 }
